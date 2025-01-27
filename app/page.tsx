05ec80d4c7b579c8f253ca8ff7fa9dbd3c446bae
Junle Yan <junleyan.hi@gmail.com>
2025-01-27 11:53:16 -1000

    Change landing page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { GalleryVerticalEnd } from "lucide-react";
 
 import { LoginForm } from "@/components/login-form";
 
@@ -8,14 +7,14 @@ export default async function LoginPage() {
         <div className="grid min-h-svh lg:grid-cols-2">
             <div className="flex flex-col gap-4 p-6 md:p-10">
                 <div className="flex justify-center gap-2 md:justify-start">
-                    <a href="#" className="flex items-center gap-2 font-medium">
-                        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                            <GalleryVerticalEnd className="size-4" />
-                        </div>
-                        ICS 414
-                    </a>
+                    <Image
+                        src="/spirelogo.png"
+                        alt="Logo"
+                        width={125}
+                        height={0}
+                    />
                 </div>
-                <div className="flex flex-1 items-center justify-center">
+                <div className="flex flex-1 items-center justify-center mb-12">
                     <div className="w-full max-w-xs">
                         <LoginForm />
                     </div>
