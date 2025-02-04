45a89bfa0a8bace2e811940d2a001d3b7cb4a9d8
Junle Yan <junleyan.hi@gmail.com>
2025-02-04 01:49:22 -1000

    Add routing solution for landing page and future pages

---
+++ components/sidebar.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import { Sidebar, SidebarContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+
+import AppSidebarContent from "./sidebar-content"
+import { auth } from "@/auth";
+import { House } from "lucide-react";
+
+const AppSidebar = async () => {
+    const session = await auth();
+    return (
+        <Sidebar collapsible="icon">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <a href="/user/home">
+                                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                                    <House className="size-4" />
+                                </div>
+                                <div className="flex flex-col gap-0.5 leading-none">
+                                    <span className="font-semibold text-l">Home</span>
+                                </div>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <AppSidebarContent role={session?.user?.role || null} />
+                {/**<NavProjects projects={data.projects} />*/}
+            </SidebarContent>
+            {/**
+                <SidebarFooter>
+                <NavUser user={data.user} />
+            </SidebarFooter>
+            */}
+
+        </Sidebar>
+    )
+}
+
+export default AppSidebar;
