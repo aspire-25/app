5e13fe74fa06a6d9f9f6ae721d95757e39e092ee
micaiahcape <mchlcape808@gmail.com>
2025-03-11 00:46:27 -1000

    Added frontend and formulas of Stress Test 2

--- components/home/analyst/stresstest1/stress-test-1.tsx
+++ components/home/analyst/stresstest1/stress-test-1.tsx
@@ -1,5 +1,7 @@
 import StressTestInputs from "./stress-tests-inputs"
+import LossOfInterestTable from "../lossOfInterestTable";
 import { useState } from "react"
+import TableDivider from "../tableDivider";
 import {
     Table,
     TableBody,
@@ -15,7 +17,6 @@ for (let i = 0; i < 12; i++) {
     years.push(Number(currentDate.getFullYear()) + i)
 }
 
-let annualReturnRate = 6.02
 
 console.log(years)
 
@@ -54,24 +55,14 @@ const StressTest1 = () => {
         }
     }
 
-    const computeColTotals = (colID: number) => {
-        let sum = 0;
-        stDiffs.forEach((item, rowID) => {
-            if (colID >= rowID) {
-                sum += (item*(Math.pow(1+annualReturnRate/100, colID-rowID+1)) - item)
-            }
-        })
-        return sum;
-    }
-
     return (
         <>
 
             {/* inputs for ST 1 */}
             <StressTestInputs onParamsUpdate={sendToParent}/>
-            <div style={{backgroundColor: "lightgray", height: "1vh", width: "100%", margin: "5% auto 5% auto"}}></div>
+            <TableDivider/>
             <StressTestInputs onParamsUpdate={sendToParent2}/>
-            <div style={{backgroundColor: "lightgray", height: "1vh", width: "100%", margin: "5% auto 5% auto"}}></div>
+            <TableDivider/>
 
             {/* summary table */}
             <Table>
@@ -94,64 +85,7 @@ const StressTest1 = () => {
             <div style={{backgroundColor: "lightgray", height: "1vh", width: "100%", margin: "5% auto 5% auto"}}></div>
 
             {/* loss of interests table */}
-            <Table>
-                <TableHeader>
-
-                    {/* row(s) for table headers */}
-                    <TableRow className="text-base">
-                        <TableHead>Year</TableHead>
-                        {years.map((item, index) => (
-                            <TableHead className="text-center font-bold text-black-900" key={index}>{item}</TableHead>
-                        ))}
-                    </TableRow>
-                </TableHeader>
-                    
-                <TableBody>
-                    {/* row(s) for other headers */}
-                    <TableRow className="text-base">
-                        <TableHead className="w-10 text-amber-900">Principal</TableHead>
-                        {years.map((item, index) => (
-                            <TableHead className="text-center font-bold text-sm text-amber-900" key={index}>${stDiffs[index].toFixed(0)}</TableHead>
-                        ))}
-                    </TableRow>
-                    <TableRow className="text-base">
-                        <TableHead className="text-amber-900">Annual Return Rate</TableHead>
-                        {years.map((item, index) => (
-                            <TableHead className="text-center font-bold text-sm text-amber-900" key={index}>{annualReturnRate}%</TableHead>
-                        ))}
-                    </TableRow>
-
-                    {/* row(s) for interests lost */}
-                    {/* <p>{stDiffs.toString()}</p> */}
-                    {stDiffs.map((item, index) => (
-                        ((index < years.length) && (
-                        <TableRow key={index}>
-                            {/* Create a row */}
-                            <TableCell className="text-amber-900 font-bold text-sm">${(item).toFixed(0)}</TableCell>
-                            {stDiffs.map((colItem, colIndex) => (
-                                ((colIndex < years.length) && (
-                                    ((colIndex >= index) ? 
-                                        <TableCell key={colIndex} className="text-xs text-center">
-                                            ${(item*(Math.pow(1+annualReturnRate/100, colIndex-index+1)) - item).toFixed(0)}
-                                        </TableCell> : <TableCell key={colIndex}></TableCell>
-                                    )
-                                ))
-                            ))}
-                        </TableRow>))       
-                    ))}
-
-                    {/* last row for total interests lost. wrap in TableHeader to get bold effect*/}
-                        <TableRow className="text-red-900">
-                            <TableCell className="text-base">Total Interest Lost</TableCell>
-                            {stDiffs.map((item, index) => (
-                                ((index < years.length) && (
-                                    <TableCell className="text-center text-sm font-bold" key={index}>${computeColTotals(index).toFixed(0)}</TableCell>
-                                ))
-                            ))}
-                        </TableRow>
-                        
-                    </TableBody>
-            </Table>
+            <LossOfInterestTable principalData={stDiffs}/>
         </>
     )
 }
