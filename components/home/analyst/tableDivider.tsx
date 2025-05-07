aef5388e6cdab14d74fb0f3e72a703e55dea7bc0
micaiahcape <mchlcape808@gmail.com>
2025-05-07 00:17:48 -1000

    made stress tests better looking

--- components/home/analyst/tableDivider.tsx
+++ components/home/analyst/tableDivider.tsx
@@ -1,6 +1,6 @@
 const TableDivider = () => {
     return (
-        <div style={{backgroundColor: "lightgray", height: "1vh", width: "100%", margin: "2% auto 2% auto"}}></div>
+        <div style={{backgroundColor: "lightgray", height: "1vh", width: "100%", margin: "1.5% auto 1.5% auto"}}></div>
     )
 }
 
