7325dd842e34f0518f81f420351ff25910fb93ad
Michaella Villanueva <mvchaella@gmail.com>
2025-05-07 03:30:15 -1000

    pages cleaned

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,20 +1,33 @@
 'use client';
 
-import AuditorHome from "../../components/home/auditor/auditor-home-adjusted";
 import AnalystHome from "../../components/home/analyst/analyst-home";
 import ExecutiveHome from "../../components/home/executive/executive-home";
 import UsersPage from "../../components/home/admin/admin-home";
+import { useLayoutEffect, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import AuditorHomeAdjusted from "@/components/home/auditor/auditor-home-adjusted";
 
 const Page = ({ tempRole }: { tempRole: string }) => {
-    return (
-        <div className="pt-[80px]"> 
-            {tempRole === "auditor" && <AuditorHome />}
-            {tempRole === "analyst" && <AnalystHome />}
-            {tempRole === "executive" && <ExecutiveHome />}
-            {tempRole === "admin" && <UsersPage />}
-        </div>
-    );
-};
+  const router = useRouter();
 
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    if (tempRole !== "auditor") {
+      router.push("/user");
+    }
+  }, [tempRole, router]);
+
+  return (
+    <div className="pt-[80px]">
+      {tempRole === "auditor" && <AuditorHomeAdjusted />}
+      {tempRole === "analyst" && <AnalystHome />}
+      {tempRole === "executive" && <ExecutiveHome />}
+      {tempRole === "admin" && <UsersPage />}
+    </div>
+  );
+};
 
 export default Page;
