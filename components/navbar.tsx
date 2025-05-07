fa5ad3f523639a946fab72a70b513807f1b2b595
Michaella Villanueva <mvchaella@gmail.com>
2025-05-06 21:23:04 -1000

    aesthetics navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from "react";
 import { getSession, Session } from "next-auth/react";
 import NavbarSettings from "@/components/navbar-settings";
-import ViewSwitcher from "@/components/toggle-view"; 
 
 const Navbar = () => {
   const [session, setSession] = useState<Session | null>(null);
@@ -13,7 +12,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="bg-blue-500/50 backdrop-blur-md p-4 text-white flex items-center justify-between fixed w-full top-0 left-0 z-50">
+    <nav className="fixed top-0 left-0 z-50 w-full bg-blue-500/30 backdrop-blur-lg border-b border-white/20 p-4 text-white flex items-center justify-between">
       <div className="flex items-center">
         <img src="/spire-logo.svg" alt="Spire Logo" className="h-10 mr-4" />
         <div className="text-xl font-semibold">Hi</div>
