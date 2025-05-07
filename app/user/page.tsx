c2008ecd5da43bb287f47c7bb9d2f74fdbdecb13
Junle Yan <junleyan.hi@gmail.com>
2025-05-07 09:05:06 -1000

    Fix build error

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,8 +1,4 @@
 import Page from "@/components/client-page";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import SpireHeader from "@/components/spire-header";
 
 const Home = () => {
     return (
